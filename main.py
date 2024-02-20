dd1bc661329372b5f481e2e9fadeed21e9bf6a1b
SebastianBarrenechea <102995074+SebastianBarrenechea@users.noreply.github.com>
2024-02-20 12:28:29 -0500

    Actualizar main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+humedad = 0
+temperatura = 0
+spo2 = 0
+pulso = 0
+glucosa = 0
 
+def on_forever():
+    global temperatura, humedad, spo2, pulso, glucosa
+    dht11_dht22.query_data(DHTtype.DHT11, DigitalPin.P0, True, False, True)
+    temperatura = dht11_dht22.read_data(dataType.TEMPERATURE)
+    humedad = dht11_dht22.read_data(dataType.HUMIDITY)
+    basic.show_string("T: " + str(temperatura) + " H: " + str(humedad))
+    basic.pause(2000)
+    spo2 = Math.random_range(90, 100)
+    basic.show_string("SpO2: " + str(spo2))
+    basic.pause(2000)
+    pulso = Math.random_range(60, 100)
+    basic.show_string("Pulso: " + str(pulso))
+    basic.pause(2000)
+    glucosa = Math.random_range(80, 140)
+    basic.show_string("Glucosa: " + str(glucosa))
+    basic.pause(2000)
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
-let humedad = 0
 let temperatura = 0
+let humedad = 0
 let spo2 = 0
 let pulso = 0
 let glucosa = 0
@@ -13,15 +13,15 @@ basic.forever(function () {
     )
     temperatura = dht11_dht22.readData(dataType.temperature)
     humedad = dht11_dht22.readData(dataType.humidity)
-    basic.showString("T: " + temperatura.toString() + " H: " + humedad.toString())
+    basic.showString("T: " + ("" + temperatura) + " H: " + ("" + humedad))
     basic.pause(2000)
     spo2 = Math.randomRange(90, 100)
-    basic.showString("SpO2: " + spo2.toString())
+    basic.showString("SpO2: " + ("" + spo2))
     basic.pause(2000)
     pulso = Math.randomRange(60, 100)
-    basic.showString("Pulso: " + pulso.toString())
+    basic.showString("Pulso: " + ("" + pulso))
     basic.pause(2000)
     glucosa = Math.randomRange(80, 140)
-    basic.showString("Glucosa: " + glucosa.toString())
+    basic.showString("Glucosa: " + ("" + glucosa))
     basic.pause(2000)
 })
